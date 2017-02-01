37d919fcdc1894b5fd9e45f1c307b23b74fb03c6
Jared Fuller <jaredshane@gmail.com>
2017-02-01 10:02:34 -0600

    picture from database posts to the page now

--- app/controllers/homectrl.js
+++ app/controllers/homectrl.js
@@ -3,6 +3,9 @@ app.controller('HomeCtrl', function() {
 
   // Create a root reference
   let storageRef = firebase.storage().ref();
+  let gsReference = firebase.storage().refFromURL('gs://fir-storage-e8e40.appspot.com/')
+
+  console.log(storageRef.child('images'))
 
   let inputElement = document.getElementById('fileInput');
 
@@ -11,9 +14,26 @@ app.controller('HomeCtrl', function() {
   function handleFiles() {
     var fileList = this.files;
     console.log("filelist[0]", fileList[0])
-    storageRef.child(fileList[0].name).put(fileList[0])
+    storageRef.child("images/" + fileList[0].name).put(fileList[0])
       .then(function(snapshot) {
         console.log('Uploaded a file!')
+        console.log(snapshot)
       })
   }
+
+
+  storageRef.child('pugpup.jpg').getDownloadURL()
+    .then(function(url) {
+    var img = document.getElementById('myImg')
+    img.src = url;
+    }).catch(function (error) {
+
+  })
+
+
+
+
+
+
+
 })
